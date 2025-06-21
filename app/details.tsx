f6911552d49a6344cfbb652f0c37fbad394794ea
Noctyris <noctyris@proton.me>
2025-06-21 19:17:19 +0200

    Update details page

--- app/details.tsx
+++ app/details.tsx
@@ -1,18 +1,24 @@
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function DetailsScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Details</Text>
-    </View>
-  );
+	const texts = [
+		"Pages pour chaque chapitre livre astro",
+		"Page index.tsx: dashboard sur l'actu scientifique"
+	]
+	
+
+	return (
+		<View style={styles.container}>
+			<Text style={{fontSize: 24}}>Details</Text>
+			{texts.map((t) => <Text key={t.slice(3)} style={{marginBottom: 3}}>- {t}</Text>)}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+	container: {
+		flex: 1,
+		padding: 10,
+	},
 });
 
